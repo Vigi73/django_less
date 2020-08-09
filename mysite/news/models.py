76ea898666470492b575b887a7dc635454db37d1
Vigi73 <vigi73@mail.ru>
2020-08-09 23:02:18 +0700

    add templates+

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class News(models.Model):
 
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150, verbose_name='Название')
     content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Публикация')
 
     def __str__(self):
         return self.title
