072cc9b21b39a58ce302f5035410fd73dfebddcb
Vigi73 <vigi73@mail.ru>
2020-08-09 21:17:38 +0700

    add templates

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class News(models.Model):
+
     title = models.CharField(max_length=150)
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -11,5 +12,11 @@ class News(models.Model):
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at']
+
 
 
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import News
 
 
 def index(request):
-    return HttpResponse('<h1>Hello World!</h1>')
-
-
+    news = News.objects.all()
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request, 'news/index.html', context=context)
