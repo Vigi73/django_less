9376018baa1074679d858f43783a8a7abc55cb28
Vigi73 <vigi73@mail.ru>
2020-08-10 15:31:00 +0700

    add bootstrap

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -9,6 +9,7 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Публикация')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
     def __str__(self):
         return self.title
@@ -19,4 +20,13 @@ class News(models.Model):
         ordering = ['-created_at']
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
